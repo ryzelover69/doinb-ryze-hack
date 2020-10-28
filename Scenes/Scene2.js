9c17b0826963e240dd6242ed02e4333462915e99
ryzelover69 <albert.schlafmann@gmail.com>
2020-10-27 20:31:55 -0500

    now with less ear destruction, but it's up to 420 to finish his stuff

--- Scenes/Scene2.js
+++ Scenes/Scene2.js
@@ -1,31 +1,27 @@
-class Scene2 extends Phaser.Scene{
-  constructor(){
-    super("playGame");
+class Scene2 extends Phaser.Scene {
+  constructor () {
+    super('game')
   }
-  create(){
-    console.log("game started");
 
-    this.background = this.add.image(0,0,"background").setOrigin(0);
+  create () {
+    console.log('game started')
+    this.background = this.add.image(0, 0, 'background').setOrigin(0)
 
     // Creates an fps counter variable, will run through update function
     var fpsText = this.add.text(10, 10, 'FPS: ', {
       font: 'bold 12px Arial',
       fill: '#ffffff'
-    });
+    })
 
     // This creates the song, plays and loops it.
-    let theme_song = this.sound.add('DOINBTHEME');
-    let theme_song_config = {mute: false, volume:0.01, loop:true};
-    theme_song.play(theme_song_config);
-    createMeleeMinions();
+    const themeSong = this.sound.add('DOINBTHEME')
+    const themeSongConfig = {mute: false, volume: 0.01, loop: true }
+    themeSong.play(themeSongConfig)
+    // test melee minon
+    createMeleeMinions()
+  };
 };
-
-
-
-
-
-function createMeleeMinions(){
-  var melee_minon = melee_minon.create(x * 48, y * 50, 'melee_source');
-  console.log("melee minon created");
-}
+function createMeleeMinions () {
+  var meleeMinion = this.create(50, 50, 'melee_source')
+  console.log('melee minion created')
 };
